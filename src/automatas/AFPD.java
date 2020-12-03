dc53546cd7b3c7279c4602b97429963092aa7488
hazmun2000 <52558171+hazmun2000@users.noreply.github.com>
2020-12-03 01:00:48 -0500

    Creacion del AFPD con respectivo test

---
+++ src/automatas/AFPD.java
@@ -0,0 +1,110 @@
+package src.automatas;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class AFPD extends Automata {
+
+    Stack<String> stack;
+
+    public AFPD(ArrayList<String> estados, String estadoInicial, ArrayList<String> estadosAceptacion,
+            ArrayList<String> alfabetoEntrada, ArrayList<String> alfabetopila, ArrayList<String> transiciones) {
+        super(alfabetoEntrada, estados, estadoInicial, estadosAceptacion, transiciones);
+        this.alfabetoPila = alfabetopila;
+        this.extension = ".pda";
+    }
+
+    public AFPD(String nombreArchivo) {
+        super(nombreArchivo.contains(".pda") ? nombreArchivo : "default.pda");
+    }
+
+    @Override
+    public String procesarCadenaTexto(String cadena) {
+        String proceso = "";
+        this.stack = new Stack<>();
+        this.stack.add("$");
+        if (!verificarAlfabetoSigma(cadena)) {
+            System.out.println("Cadena no cumple con el alfabeto");
+            return "No cumple con el alfabeto Sigma";
+        }
+        String estadoActual = this.estadoInicial;
+
+        while (encontrarTransicion(estadoActual,
+                (!cadena.isEmpty() ? String.valueOf(cadena.charAt(0)) : " ")) != null) { // Mientras la transicion exista
+            if (verificarAlfabetoGamma(mostrarPila())) {
+
+                proceso += "(" + estadoActual + "," + cadena + "," + mostrarPila() + ")->";
+
+                String transicion = encontrarTransicion(estadoActual,
+                        (!cadena.isEmpty() ? String.valueOf(cadena.charAt(0)) : " "));
+
+                modificarPila(this.stack, transicion.split(":")[2],
+                        this.transiciones.get(transicion).get(0).split(":")[1]);
+
+                estadoActual = this.transiciones.get(transicion).get(0).split(":")[0];
+                
+                //si hay una trasicion tipo q:$ no modifica la cadena
+                if (!transicion.split(":")[1].equals("$")) {
+                    cadena = cadena.substring(1);
+                }
+
+                transicion = encontrarTransicion(estadoActual,
+                        (!cadena.isEmpty() ? String.valueOf(cadena.charAt(0)) : " "));
+
+            } else {
+                return "No cumple con el alfabeto Gamma";
+            }
+        }
+
+        // Termina de procesar la cadena
+        proceso += "(" + estadoActual + "," + (cadena.isEmpty() ? "$" : cadena) + ","
+                + (this.stack.peek().equals("$") ? "$" : mostrarPila()) + ")>>";
+        // Mira si termino en un estado de aceptacion
+        return proceso + (this.estadosAceptacion.contains(estadoActual)
+                && cadena.isEmpty()
+                && this.stack.peek().equals("$") ? "accepted" : "rejected");
+
+    }
+
+    void modificarPila(Stack s, String operecion, String parametro) {
+        if (operecion.equals("$")) {
+            if (!parametro.equals("$")) {
+                s.push(parametro); // Añade
+            }
+        } else {
+            if (!stack.peek().equals("$")) {
+                s.pop(); // Elimina
+                if (!parametro.equals("$")) {
+                    s.push(parametro); // Remplaza
+                }
+            }
+        }
+
+    }
+
+    String encontrarTransicion(String estadoActual, String cabezaCinta) {
+        //intenta encontrar llave con la forma estadoActia:$:alfabetopila
+        //ej q0:$:A
+        for (String s : this.alfabetoPila) {
+            if (this.transiciones.get(estadoActual + ":$:" + s) != null) {
+                return (estadoActual + ":$:" + s);
+            }
+        }
+        //intenta encontrar llave con la forma estadoActia:cabezaCinta:alfabetoPila
+        //ej q0:b:A
+        for (String s : this.alfabetoPila) {
+            if (this.transiciones.get(estadoActual + ":" + cabezaCinta + ":" + s) != null) {
+                return (estadoActual + ":" + cabezaCinta + ":" + s);
+            }
+        }
+        return null;
+    }
+
+    String mostrarPila() {
+        String out = "";
+        for (String s : this.stack) {
+            out += s;
+        }
+        return out;
+    }
+}
---
+++ test/automatas/AFPDTest.java
@@ -0,0 +1,68 @@
+package automatas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.junit.*;
+
+import src.automatas.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class AFPDTest {
+
+    AFPD afpd;
+    HashMap<String, Boolean> cadenasExpected;
+
+    public void crearafpd() {
+        // Test afpd (ab U ba)*
+        // Creacion del automata basico para pruebas
+        ArrayList<String> alfabetoEntrada = new ArrayList<>(Arrays.asList("a", "b"));
+        ArrayList<String> alfabetoPila = new ArrayList<>(Arrays.asList("$", "A"));
+        ArrayList<String> estados = new ArrayList<>(Arrays.asList("q0", "q1", "q2"));
+        String estadoInicial = "q0";
+        ArrayList<String> estadosAceptacion = new ArrayList<>(Arrays.asList("q0", "q2"));
+        ArrayList<String> delta = new ArrayList<String>(Arrays.asList("q0:a:$~q1:A", "q0:b:A~q2:$", "q1:a:$~q0:$", "q2:b:A~q2:$"));
+        afpd = new AFPD(estados, estadoInicial, estadosAceptacion, alfabetoEntrada, alfabetoPila, delta);
+
+        // Todas las cadenas y su resultado esperado parael automata basico de pruebas
+        cadenasExpected = new HashMap<>();
+        cadenasExpected.put("abb", true);
+        cadenasExpected.put("aaabb", false);
+        cadenasExpected.put("bbbaa", false);
+        cadenasExpected.put("babababa", false);
+    }
+
+    @Test
+    public void procesarCadenaCodigo() {
+        // Test Constructor(alfabeto, estados, estadoInicial, estadosAceptacion,delta)
+        crearafpd();
+        // Test Booleano procesarCadena(cadena)
+        for (Map.Entry<String, Boolean> pair : this.cadenasExpected.entrySet()) {
+            assertEquals(pair.getValue(), this.afpd.procesarCadena(pair.getKey()));
+        }
+    }
+
+    @Test
+    public void procesarCadenaArchivo() {
+        // Test Constructor(nombreArchivo)
+        this.afpd = new AFPD("default.pda");
+        // Test Booleano procesarCadena(cadena)
+        assertEquals(true, this.afpd.procesarCadena("abb")); // Cadena aceptada
+        assertEquals(false, this.afpd.procesarCadena("aaabb")); // Cadena no aceptada
+        assertEquals(false, this.afpd.procesarCadena("bbbaa")); // No termina en estado de aceptacion
+        assertEquals(false, this.afpd.procesarCadena("babababa")); // No termina en estado de aceptacion
+    }
+
+    @Test
+    public void procesarCadenaConDetalles() {
+        // Test Booleano procesarCadenaConDetalles(cadena)
+        crearafpd();
+        for (Map.Entry<String, Boolean> pair : this.cadenasExpected.entrySet()) {
+            assertEquals(pair.getValue(), this.afpd.procesarCadena(pair.getKey()));
+        }
+    }
+}
